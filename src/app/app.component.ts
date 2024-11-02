bc074fd7dc027fa26b3c96b469328618f38cfc7c
josepereza <josepereza66@gmail.com>
2024-11-02 17:14:28 +0100

    first commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
+import { GridListComponent } from './components/grid-list/grid-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent, GridListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
